0c3e354c123e85cf7fa3ae48df71413eb7d494e5
Meg Prescott <mprescott@ccsnh.edu>
2015-04-20 21:17:24 -0400

    First phase of run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,14 @@
-#
+#load all libraries needed throughout
+library(reshape2)
+
+#combine subject files to provide one of the record identifiers
+subject <- c(
+  read.table('./UCI HAR Dataset/test/subject_test.txt')[,'V1'],
+  read.table('./UCI HAR Dataset/train/subject_train.txt')[,'V1']
+)
+
+#similarly combine the activity codes found in the y_test.txt and the y_train.txt files
+y_codes <- c(
+  read.table('./UCI HAR Dataset/test/y_test.txt')[,'V1'],
+  read.table('./UCI HAR Dataset/train/y_train.txt')[,'V1']
+)
